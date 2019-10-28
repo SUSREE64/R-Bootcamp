b72a31ba94c9b3da171a1e068114ec9d1dbdebf6
SUSREE64 <ssvipl64@gmail.com>
2019-10-28 16:27:35 +0530

    commit

---
+++ R-Basics/R-Basic Datatypes.R
@@ -0,0 +1,10 @@
+#Basic Data types
+#Numeric type
+a <- 2.0
+b <- 3
+#Character type
+c <- 'sree'
+# to check the class of the  datatypes
+class(a)
+class(b)
+class(c)
---
+++ R-Basics/R-Variables.R
@@ -0,0 +1,12 @@
+# Variables and assignment of variables
+# Variables should sart with a small letter
+bank.balance <- 1000 # Notice the . in the variable name, <- assignment operator
+bank.withdraw <- 20
+bank.deposit <- 200
+bank.balance <- bank.balance+bank.deposit-bank.withdraw
+print(bank.balance)
+# Variables also can be assigned with values using "="
+# But this not the convension 
+num1 = 20
+num2 = 40
+print(num1+num2)
