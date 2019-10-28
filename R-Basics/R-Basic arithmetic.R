a1d7eb8b57365c130199c389bfd75ec68e05f039
SUSREE64 <ssvipl64@gmail.com>
2019-10-28 13:15:41 +0530

    Create R-Basic arithmetic.R

---
+++ R-Basics/R-Basic arithmetic.R
@@ -0,0 +1,18 @@
+# This is a comment line
+#Basic arithmetic with r
+# Add
+1+2
+#subtract
+3-4
+#multiply
+2*5
+#modulo - Note double %% sign
+10 %% 3
+#division
+10/3
+#float operations
+1.345 - 2.456
+# Power
+2**4
+# Power 
+2^4
