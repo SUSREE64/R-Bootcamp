5d1527c49673271016b352e98a224f7010bd5bd5
SUSREE64 <ssvipl64@gmail.com>
2019-10-28 19:36:18 +0530

    commit
    commit

---
+++ R-Basics/R-Vector operations.R
@@ -0,0 +1,10 @@
+# Operations on the vectors
+vec1 <- c(1,2,3,5)
+print(vec1*5)
+print("Each element is multiplied with 5 and printed")
+vec2 <- c(10,20,30,50)
+print (vec1+vec2)
+print('Element to element addition takes place')
+# Some function on the vectors
+print("Length of number of elements in vec1")
+length(vec1)
